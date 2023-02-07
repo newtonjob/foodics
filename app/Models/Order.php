7d79f573983dd32da61b76df685356fd344e5bc1
newtonjob <jobnewton3@gmail.com>
2023-02-07 11:23:37 +0100

    Unguard the `Order` model and define the `products()` relationship

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,8 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Order extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that aren't mass assignable.
+     */
+    protected $guarded = [];
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class)->withTimestamps();
+    }
 }
