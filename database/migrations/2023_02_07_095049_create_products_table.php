b3275a9852fcfbcc0cf56cbc806759e16693f4ac
newtonjob <jobnewton3@gmail.com>
2023-02-07 11:07:10 +0100

    Add `price` column to `products` table

--- database/migrations/2023_02_07_095049_create_products_table.php
+++ database/migrations/2023_02_07_095049_create_products_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
+            $table->float('price');
             $table->timestamps();
         });
     }
