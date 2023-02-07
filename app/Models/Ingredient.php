b5c779ff86ee8b6ac980d87553753c80c73ac787
newtonjob <jobnewton3@gmail.com>
2023-02-07 12:09:21 +0100

    Create helper methods `lowStockAlertHasBeenPreviouslySent()` and shouldSendLowStockAlert() on `Ingredient` model

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -13,4 +13,24 @@ class Ingredient extends Model
      * The attributes that aren't mass assignable.
      */
     protected $guarded = [];
+
+    /**
+     * Determine if the ingredient stock is low and a low stock notification should be sent.
+     */
+    public function shouldSendLowStockAlert(): bool
+    {
+        if ($this->lowStockAlertHasBeenPreviouslySent()) {
+            return false;
+        }
+
+        return $this->stock <= $this->low_stock_threshold;
+    }
+
+    /**
+     * Determine if a low stock notification has been previously sent for this ingredient.
+     */
+    public function lowStockAlertHasBeenPreviouslySent(): bool
+    {
+        return (bool) $this->low_stock_alert_sent_at;
+    }
 }
