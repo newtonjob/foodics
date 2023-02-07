b36034fc781adc941726bfbe8fd9ac5b9f8a9650
newtonjob <jobnewton3@gmail.com>
2023-02-07 11:55:46 +0100

    Define the `ingredients` relation on `Product` table and add helper method for `sellout()`

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Product extends Model
 {
@@ -15,4 +16,21 @@ class Product extends Model
     protected $casts = [
         'low_stock_alert_sent_at' => 'datetime'
     ];
+
+    /**
+     * Sellout from the product's ingredient stock, the quantity that was ordered.
+     */
+    public function sellout(): void
+    {
+        $quantity = $this->pivot->quantity ?? 1;
+
+        $this->ingredients->each(
+            fn ($ingredient) => $ingredient->decrement('stock', $ingredient->pivot->quantity * $quantity)
+        );
+    }
+
+    public function ingredients(): BelongsToMany
+    {
+        return $this->belongsToMany(Ingredient::class)->withTimestamps();
+    }
 }
