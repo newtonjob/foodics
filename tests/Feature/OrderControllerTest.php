909b058d3ea20450661ba08b86a0598f48d31d33
newtonjob <jobnewton3@gmail.com>
2023-02-08 12:29:22 +0100

    Wrap up the `OrderControllerTest`

---
+++ tests/Feature/OrderControllerTest.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Ingredient;
+use App\Models\Product;
+use App\Notifications\IngredientStockLow;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use Illuminate\Support\Facades\Notification;
+use Tests\TestCase;
+
+class OrderControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * Indicates whether the default seeder should run before each test.
+     *
+     * @var bool
+     */
+    protected $seed = true;
+
+    public function test_orders_can_be_placed(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->postJson(route('orders.store'), [
+            'products' => [
+                [
+                    'product_id' => 1,
+                    'quantity'   => 3
+                ]
+            ]
+        ]);
+
+        $response->assertCreated();
+        $this->assertDatabaseHas('orders', ['user_id' => $user->id]);
+    }
+
+    public function test_ingredients_stock_is_reduced_when_orders_are_created(): void
+    {
+        $user    = User::factory()->create();
+        $product = Product::with('ingredients')->first();
+
+        $this->actingAs($user)->postJson(route('orders.store'), [
+            'products' => [
+                [
+                    'product_id' => $product->id,
+                    'quantity'   => 10
+                ]
+            ]
+        ]);
+
+        $product->fresh()->ingredients->each(function ($ingredient) use ($product) {
+            $original = $product->ingredients->find($ingredient);
+
+            $this->assertLessThan($original->stock, $ingredient->stock);
+        });
+    }
+
+    public function test_notification_is_sent_once_when_ingredient_stock_is_reduced_below_threshold(): void
+    {
+        Notification::fake();
+
+        $ingredient = Ingredient::first();
+
+        $ingredient->decrement('stock', $decrement = $ingredient->stock / 2);
+        $ingredient->decrement('stock', $decrement /= 2);
+        $ingredient->decrement('stock', $decrement / 2);
+
+        Notification::assertSentOnDemandTimes(IngredientStockLow::class);
+    }
+}
