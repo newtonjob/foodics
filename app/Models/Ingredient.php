7373b20b2ddb81994473fb3d54baf462c3974d8c
newtonjob <jobnewton3@gmail.com>
2023-02-08 13:13:21 +0100

    Remove the need for observer classes and use simple closure-based model event listeners

--- app/Models/Ingredient.php
+++ app/Models/Ingredient.php
@@ -3,16 +3,27 @@
 namespace App\Models;
 
 use App\Models\Traits\InteractsWithIngredientStock;
-use App\Observers\HasObserver;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Ingredient extends Model
 {
-    use HasFactory, HasObserver, InteractsWithIngredientStock;
+    use HasFactory, InteractsWithIngredientStock;
 
     /**
      * The attributes that aren't mass assignable.
      */
     protected $guarded = [];
+
+    /**
+     * Perform any actions required after the model boots.
+     */
+    protected static function booted()
+    {
+        static::updated(function (Ingredient $ingredient) {
+            if ($ingredient->wasChanged('stock') && $ingredient->shouldSendLowStockAlert()) {
+                $ingredient->sendLowStockAlert();
+            }
+        });
+    }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,19 +2,28 @@
 
 namespace App\Models;
 
-use App\Observers\HasObserver;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Order extends Model
 {
-    use HasFactory, HasObserver;
+    use HasFactory;
 
     /**
      * The attributes that aren't mass assignable.
      */
     protected $guarded = [];
+
+    /**
+     * Perform any actions required after the model boots.
+     */
+    protected static function booted()
+    {
+        static::created(function (Order $order) {
+            dispatch(fn () => $order->products->each->sellout())->afterResponse();
+        });
+    }
 
     public function products(): BelongsToMany
     {
